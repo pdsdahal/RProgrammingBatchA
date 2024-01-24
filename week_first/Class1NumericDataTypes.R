9dc28f0eeb1720ca3b0ea0ca546ee6f78cbb9f77
Shankar Pd. Dahal <pdsdahal@gmail.com>
2024-01-24 15:30:11 -0600

    Numeric Data Types in R

---
+++ week_first/Class1NumericDataTypes.R
@@ -0,0 +1,17 @@
+#Numeric Data Types 
+
+#to represent integer use the letter L after the value assigned for e.g.
+
+#Integer
+age <- 11L
+is.numeric(age) #TRUE
+is.integer(age) #TRUE
+typeof(age) #to check the data type of a variable
+class(age) #to check the data type of a variable
+
+
+#double
+num1 <- 10
+is.numeric(num1) #TRUE
+is.integer(num1) #FALSe
+typeof(num1) #double
