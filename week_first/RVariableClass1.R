5c7dabe2016c033df9244e8dd042fbc7959f0ee8
Shankar Prasad Dahal <sdahal@Shankars-MBP.lan>
2024-01-24 14:05:50 -0600

    R Variable Discussion

---
+++ week_first/RVariableClass1.R
@@ -0,0 +1,19 @@
+#R- variable creation 
+
+#starting with undrscore(_) is not a valid variable
+#_collegeName = "Texas"
+
+#Starting with number is not a valid variable
+#1data <- 1000
+
+#Vairable name should be meaning full. I recommend to use camel case like below. 
+
+collegeName <- "ABC College"
+collegeName
+
+num1 <- 10L
+num1
+
+.flagValue = TRUE
+.flagValue
+
