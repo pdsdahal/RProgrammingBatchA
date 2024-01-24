d1080d44124cc314d680f143b2ab1dd2c15d5a70
Shankar Pd. Dahal <pdsdahal@gmail.com>
2024-01-24 15:39:50 -0600

    Character Data Types in R

---
+++ week_first/Class1CharacterDataTypes.R
@@ -0,0 +1,8 @@
+# R- Character DataTypes
+
+countryName <- "United States"
+is.character(countryName) #TRUE, it checks whether the variable countryName is a character type.
+typeof(countryName) #character, function to check the data type of a variable:
+class(countryName) #character, function to check the data type of a variable
+
+nchar(countryName) # length of character string in a variable use in the nchar
