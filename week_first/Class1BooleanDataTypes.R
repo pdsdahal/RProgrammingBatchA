d0df310140a136e49360747bee40168b63531691
Shankar Pd. Dahal <pdsdahal@gmail.com>
2024-01-24 19:59:21 -0600

    R Boolean DataTypes

---
+++ week_first/Class1BooleanDataTypes.R
@@ -0,0 +1,9 @@
+
+isMaleGender <- TRUE
+
+#To check whether a variable is of logical type (representing Boolean values)
+is.logical(isMaleGender)
+
+# function to check the data type of a variable
+typeof(isMaleGender)
+class(isMaleGender)
