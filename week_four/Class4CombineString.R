9ba5060751ba0f0222dfef6e96f67abdeaae2f69
Shankar Prasad Dahal <49245870+pdsdahal@users.noreply.github.com>
2024-02-19 17:29:11 -0600

    Updated String Manipulation in R
    Updated String Manipulation in R

--- week_four/Class4CombineString.R
+++ week_four/Class4CombineString.R
@@ -8,6 +8,20 @@ empAddress <- "USA"
 empPhoneNo <- "12345678"
 paste(emplName, empAddress, empPhoneNo, "Texas")
 
+# element can be a vector of data to be put together.
+universityNames <- c("Texas University", "St. Louis University", "Oxford University")
+universityPhoneNo <- c("123456","876543","567890")
+paste(universityNames, universityPhoneNo, sep = " Phone No : ")
+
+# [1] "Texas University Phone No : 123456"     "St. Louis University Phone No : 876543"
+# [3] "Oxford University Phone No : 567890"   
+
+#When the vectors do not have the same length, they are recycled.
+professorNames <- c("Todd","Zim","Jack")
+professorAddres <- c("Nottingham", "St. Louis")
+paste("Hello", professorNames, professorAddres)
+
+
 #2. sprintf
 #long string with placeholders for inserting values
 
